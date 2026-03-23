aa3cec3cc372ad9596ae967546b65d0276deb38e
Jake Stothard <stothardj@gmail.com>
2026-03-22 23:19:24 -0700

    Define layer.

--- src/micrograd/nn.clj
+++ src/micrograd/nn.clj
@@ -8,7 +8,7 @@
   Module
   (parameters [this] (conj w b)))
 
-(defn make-neuron [nin nonlin]
+(defn make-neuron [nin & {:keys [nonlin] :or {nonlin true}}]
   (->Neuron (repeatedly nin #(core/make-value (- (rand 2) 1)))
             (core/make-value 0)
             nonlin))
@@ -18,3 +18,13 @@
              (reduce core/add)
              (core/add (:b n)))]
     (if (:nonlin n) (core/relu lin) lin)))
+
+(defrecord Layer [neurons]
+  Module
+  (parameters [this] (mapcat parameters neurons)))
+
+(defn make-layer [nin nout & {:keys [nonlin] :as opts}]
+  (->Layer (repeatedly nout #(apply make-neuron (conj (mapcat identity opts) nin)))))
+
+(defn apply-layer [l x]
+  (map #(apply-neuron % x) (:neurons l)))
